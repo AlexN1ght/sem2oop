5a543330320df67760f75e16b00daf5282301f60
AlexN1ght <alexiscom@icloud.com>
2019-09-06 13:47:22 +0000

    init

---
+++ 1lr/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class BitString {
+	private:
+		unsigned long long hf1;
+		unsigned long long hf2;
+	public:
+		BitString operator=(const
+}
+
+int main(void)
+{
+	
+}
